966f4eaf174078e5e0c1c1278a6fe3a4963a472a
pushpithab <pushpitha.sarasa@gmail.com>
2026-07-31 12:32:04 +0530

    comment test

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ while (running)
     Console.WriteLine("4. Toggle done");
     Console.WriteLine("5. Delete task");
     Console.WriteLine("6. Exit");
-    Console.Write("Choose an option: ");
+    Console.Write("Choose an option: "); //COMMENT pushpithaDev
 
     var choice = Console.ReadLine();
 
